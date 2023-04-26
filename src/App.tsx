41b762e8901b5ab9fa31b54722576c857d2efc81
Alexander <mrreads@yandex.com>
2023-04-26 17:45:00 +0300

    feature center additional text

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
               <div className={styles.features}>
                 <Feature topText="мы" centerText="1" bottomText="на рынке" hoverPosition="top-left" />
                 <Feature topText="гарантируем" centerText="50" bottomText="безопасность" hoverPosition="top-right" />
-                <Feature topText="календарик за" centerText="2001" bottomText="в подарок" hoverPosition="bottom-left" />
+                <Feature topText="календарик за" centerText="2001" centerTextAdditional='г.' bottomText="в подарок" hoverPosition="bottom-left" />
                 <Feature topText="путешествие" centerText="597" bottomText="дней" hoverPosition="bottom-right" />
               </div>
             </div>
--- src/components/Feature/index.tsx
+++ src/components/Feature/index.tsx
@@ -4,13 +4,14 @@ interface IProps {
     topText: string, 
     centerText: string,
     bottomText: string,
+    centerTextAdditional?: string | null,
     hoverPosition: 'top-left' | 'top-right' | 'bottom-right' | 'bottom-left'
 }
-export default function Feature({ topText, centerText, bottomText, hoverPosition }: IProps) {
+export default function Feature({ topText, centerText, bottomText, hoverPosition, centerTextAdditional = null }: IProps) {
     return (
     <div className={styles.feature} data-hover={hoverPosition}>
         <p className={styles.top}>{topText}</p>
-        <p className={styles.center}>{centerText}</p>
+        <p className={styles.center}>{centerText} { centerTextAdditional ? <span className={styles.additional}>{ centerTextAdditional }</span> : null }</p>
         <p className={styles.bottom}>{bottomText}</p>
 
         <div className={styles.background} />
