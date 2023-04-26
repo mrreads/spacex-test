9ee96f6a77a1647a95aaa2c002af1b8653c38eea
Alexander <mrreads@yandex.com>
2023-04-27 00:34:18 +0300

    add mobile header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,28 @@
 import Logo from '@/assets/images/logo.png';
 
 import styles from './Header.module.scss';
+import { useEffect, useRef, useState } from 'react';
+
+import useOnClickOutside from "@/hooks/useOnClickOutside";
 
 export default function Header() {
+    const MOBILE_WIDTH = 1200;
+
+    const [isHeaderOpen, setHeaderOpen] = useState(false);
+    const toggleOpen = () => setHeaderOpen(!isHeaderOpen);
+
+    const refHeader = useRef(null);
+    useOnClickOutside(refHeader, () => setHeaderOpen(false));
+
+    useEffect(() => {
+        window.addEventListener('resize', resizeHandler);
+        return () => window.removeEventListener('resize', resizeHandler);
+    }, []);
+
+    const resizeHandler = () => (document.documentElement.clientWidth > MOBILE_WIDTH) ? setHeaderOpen(false) : null;
+
     return (
-        <header className={styles.header}>
+        <header ref={refHeader} className={styles.header} data-opened={isHeaderOpen}>
             <div className={`container ${styles.container}`}>
 
                 <a className={styles.logo} href="#">
@@ -21,6 +39,10 @@ export default function Header() {
                     <a href='#' className={styles.item}>О компании</a>
                     <a href='#' className={styles.item}>Контакты</a>
                 </nav>
+
+                <div className={styles.burger} onClick={toggleOpen}>
+                    <p>|||</p>
+                </div>
 
             </div>
         </header>
